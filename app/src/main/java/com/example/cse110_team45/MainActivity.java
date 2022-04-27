eb6e71ebb07c5bc60de8985e623bd8573dfc3248
matthew-mikh <mlmikhai@ucsd.edu>
2022-04-26 20:29:08 -0700

    comment out tests

--- app/src/main/java/com/example/cse110_team45/MainActivity.java
+++ app/src/main/java/com/example/cse110_team45/MainActivity.java
@@ -11,7 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent directionsTestIntent = new Intent(this,DirectionDetailsActivity.class);
-        startActivity(directionsTestIntent);
+        // Lines below for testing DirectionDetails activity
+        // Intent directionsTestIntent = new Intent(this,DirectionDetailsActivity.class);
+        // startActivity(directionsTestIntent);
     }
 }
