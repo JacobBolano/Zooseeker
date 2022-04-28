8afabb314ba84f23bbb6c38c19946d98b6570f8f
Rahil Shah <rahilshah042@gmail.com>
2022-04-27 17:56:37 -0700

    imported map assets

--- app/src/main/java/com/example/cse110_team45/plan.java
+++ app/src/main/java/com/example/cse110_team45/plan.java
@@ -1,14 +1,54 @@
 package com.example.cse110_team45;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Build;
+import android.os.Bundle;
+
+import org.jgrapht.*;
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+import org.jgrapht.graph.*;
+import org.jgrapht.nio.json.JSONImporter;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 
 import android.os.Bundle;
 
 public class plan extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plan);
+
+        // Create graph
+        Graph<String, DefaultWeightedEdge> g = null;
+        try {
+            // Reads in graph from JSON file
+            g = createGraphFromJSON(this, "res/sample_graph.json");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
+    public static Graph<String, DefaultWeightedEdge> createGraphFromJSON(Context context, String path)
+            throws IOException {
+        Graph<String, DefaultWeightedEdge> g = new SimpleWeightedGraph<>(DefaultWeightedEdge.class);
+
+        JSONImporter<String, DefaultWeightedEdge> jsonImporter = new JSONImporter<>();
+        jsonImporter.setVertexFactory(label -> label);
+
+        InputStream input = context.getAssets().open(path);
+        Reader reader = new InputStreamReader(input);
+        jsonImporter.importGraph(g, reader);
+
+        return g;
+    }
+
 }
