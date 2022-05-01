d1e6a980b6102a36c9e3571e273f21a27c9387da
JacobBolano <jacobcbolano@gmail.com>
2022-05-01 16:07:56 -0700

    Fixed bug in MainActivity

--- app/src/main/java/com/example/cse110_team45/MainActivity.java
+++ app/src/main/java/com/example/cse110_team45/MainActivity.java
@@ -12,8 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // change to search activity later
-        Intent intent = new Intent(this, plan.class);
+        Intent intent = new Intent(this, SearchActivity.class);
         startActivity(intent);
 
 
