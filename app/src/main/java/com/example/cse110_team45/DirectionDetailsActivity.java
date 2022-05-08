3d392254ca360dd3563daaec8b36af77bc7f4ea3
Jaron Zamora <koopaj@gmail.com>
2022-05-08 15:30:57 -0700

    ZenHub issue #9

--- app/src/main/java/com/example/cse110_team45/DirectionDetailsActivity.java
+++ app/src/main/java/com/example/cse110_team45/DirectionDetailsActivity.java
@@ -9,29 +9,51 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import org.jgrapht.Graph;
+import org.jgrapht.GraphPath;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class DirectionDetailsActivity extends AppCompatActivity {
 
     public RecyclerView recyclerView;
+    public TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_direction_details);
 
+        Intent intent = getIntent();
+        List<String> orderedExhibitNames = intent.getStringArrayListExtra("orderedExhibitNames");
+        List<GraphPath> orderedEdgeList = intent.getExtras().getParcelable("orderedEdgeList");
+
+        // 1. Load the graph...
+        Graph<String, IdentifiedWeightedEdge> g = ZooData.loadZooGraphJSON("sample_zoo_graph.json", this);
+
+        // 2. Load the information about our nodes and edges...
+        Map<String, ZooData.VertexInfo> vInfo = ZooData.loadVertexInfoJSON("sample_node_info.json", this);
+        Map<String, ZooData.EdgeInfo> eInfo = ZooData.loadEdgeInfoJSON("sample_edge_info.json", this);
+
         DirectionAdapter adapter = new DirectionAdapter();
+
+        textView = findViewById(R.id.directionDetails);
+        textView.setText(orderedExhibitNames.get(0));
+
+        List<MockIndividualEdge> mylist = new ArrayList<MockIndividualEdge>();
+
 
         recyclerView = findViewById(R.id.direction_lists);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(adapter);
 
         List<MockIndividualDirection> testingList = new ArrayList<MockIndividualDirection>();
-        testingList.add(new MockIndividualEdge("test street 1", 100));
-        testingList.add(new MockIndividualNode("test street intersection"));
-        testingList.add(new MockIndividualEdge("test street 2", 200));
-        adapter.setIndividualDirectionListItems(testingList);
+        //testingList.add(new MockIndividualEdge("test street 1", 100));
+        //testingList.add(new MockIndividualNode("test street intersection"));
+        //testingList.add(new MockIndividualEdge("test street 2", 200));
+        //adapter.setIndividualDirectionListItems(testingList);
 
     }
 
--- app/src/main/java/com/example/cse110_team45/MockIndividualEdge.java
+++ app/src/main/java/com/example/cse110_team45/MockIndividualEdge.java
@@ -1,11 +1,13 @@
 package com.example.cse110_team45;
 public class MockIndividualEdge implements MockIndividualDirection{
     String streetName;
+    String nodeTo;
     int distance;
 
-    public MockIndividualEdge(String name, int dist){
+    public MockIndividualEdge(String name, String end, int dist){
         streetName = name;
         distance = dist;
+        nodeTo = end;
     }
 
     @Override
