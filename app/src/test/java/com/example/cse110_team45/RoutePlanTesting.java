836485e1e73497b079e92c9f002f979507d4c068
Rahil Shah <rahilshah042@gmail.com>
2022-05-08 17:20:24 -0700

    zenhub issue #41, #42 Route Plan unit testing

--- app/src/test/java/com/example/cse110_team45/RoutePlanTesting.java
+++ app/src/test/java/com/example/cse110_team45/RoutePlanTesting.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.assertNotNull;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import androidx.annotation.RequiresApi;
@@ -27,33 +28,82 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+
 @RunWith(AndroidJUnit4.class)
 public class RoutePlanTesting {
-    static Intent intent;
+    static Intent nonEmptyIntent;
     static {
-        intent = new Intent(ApplicationProvider.getApplicationContext(), plan.class);
+        nonEmptyIntent = new Intent(ApplicationProvider.getApplicationContext(), plan.class);
         ArrayList<String> destinationList = new ArrayList<String>();
-        destinationList.add("gorillas");
-        destinationList.add("lions");
-        destinationList.add("gators");
-        Bundle bundle = new Bundle();
-        bundle.putStringArrayList("destinationList", (ArrayList<String>) destinationList);
-        intent.putExtras(bundle);
+
+        nonEmptyIntent.putStringArrayListExtra("destinationList", (ArrayList<String>) destinationList);
+    }
+
+    static Intent emptyIntent;
+    static {
+        emptyIntent = new Intent(ApplicationProvider.getApplicationContext(), plan.class);
+        ArrayList<String> destinationList = new ArrayList<String>();
+        emptyIntent.putStringArrayListExtra("destinationList", (ArrayList<String>) destinationList);
+
     }
 
 
+
+    planData PlanData;
+
     @Rule
-    public ActivityScenarioRule<plan> planTestRule = new ActivityScenarioRule<>(intent);
+    public ActivityScenarioRule<plan> nonEmptyPlanTestRule = new ActivityScenarioRule<>(nonEmptyIntent);
+
+    @Rule
+    public ActivityScenarioRule<plan> emptyPlanTestRule = new ActivityScenarioRule<>(emptyIntent);
+
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Test
-    public void basicTest() {
+    public void nonEmptyPlanTest() {
 
-        ActivityScenario scenario = planTestRule.getScenario();
+        ActivityScenario scenario = nonEmptyPlanTestRule.getScenario();
         scenario.moveToState(Lifecycle.State.CREATED);
         scenario.onActivity(activity -> {
-            TextView OrderedVisitsView = activity.findViewById(R.id.orderedVisits);
-            assertNotNull(OrderedVisitsView.getText());
+            List<String> visits = new ArrayList<String>();
+            visits.add("gorillas");
+            visits.add("lions");
+            visits.add("gators");
+            // 1. Load the graph...
+            Graph<String, IdentifiedWeightedEdge> g = ZooData.loadZooGraphJSON("sample_zoo_graph.json", activity);
+            System.out.println(visits.toString());
+            // 2. Load the information about our nodes and edges...
+            Map<String, ZooData.VertexInfo> vInfo = ZooData.loadVertexInfoJSON("sample_node_info.json", activity);
+            Map<String, ZooData.EdgeInfo> eInfo = ZooData.loadEdgeInfoJSON("sample_edge_info.json", activity);
+
+            PlanData = new planData(g, vInfo, eInfo, visits);
+            PlanData.pathFinding();
+            assertEquals(5, PlanData.orderedPathExhibitNames.size());
+            assertEquals("entrance_exit_gate", PlanData.orderedPathExhibitNames.get(0));
+            assertEquals("entrance_exit_gate", PlanData.orderedPathExhibitNames.get(PlanData.orderedPathExhibitNames.size()-1));
+        });
+    }
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Test
+    public void emptyPlanTest() {
+
+        ActivityScenario scenario = emptyPlanTestRule.getScenario();
+        scenario.moveToState(Lifecycle.State.CREATED);
+        scenario.onActivity(activity -> {
+            List<String> visits = nonEmptyIntent.getStringArrayListExtra("destinationList");
+
+            // 1. Load the graph...
+            Graph<String, IdentifiedWeightedEdge> g = ZooData.loadZooGraphJSON("sample_zoo_graph.json", activity);
+
+            // 2. Load the information about our nodes and edges...
+            Map<String, ZooData.VertexInfo> vInfo = ZooData.loadVertexInfoJSON("sample_node_info.json", activity);
+            Map<String, ZooData.EdgeInfo> eInfo = ZooData.loadEdgeInfoJSON("sample_edge_info.json", activity);
+
+            PlanData = new planData(g, vInfo, eInfo, visits);
+            PlanData.pathFinding();
+            assertEquals(2, PlanData.orderedPathExhibitNames.size());
+            assertEquals("entrance_exit_gate", PlanData.orderedPathExhibitNames.get(0));
+            assertEquals("entrance_exit_gate", PlanData.orderedPathExhibitNames.get(PlanData.orderedPathExhibitNames.size()-1));
         });
     }
 }
