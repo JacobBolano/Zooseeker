f118f63ce083a70bb53e1e40f8145aeda74c3ff0
edwardburns1 <burnsedward02@gmail.com>
2022-05-24 20:56:19 -0700

    Zenhub Issue #9 Refactored Direction Details for the requriements of the new iteration

--- app/src/main/java/com/example/cse110_team45/DirectionDetailsActivity.java
+++ app/src/main/java/com/example/cse110_team45/DirectionDetailsActivity.java
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -21,9 +22,10 @@ public class DirectionDetailsActivity extends AppCompatActivity {
 
     public RecyclerView recyclerView;
     public TextView textView;
-    List<GraphPath> orderedEdgeList;
-    List<String> orderedExhibitNames;
-    String prevNode;
+    DirectionAdapter adapter;
+
+    DirectionData directionData;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,10 +33,13 @@ public class DirectionDetailsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_direction_details);
 
         Intent intent = getIntent();
-        orderedExhibitNames = intent.getStringArrayListExtra("orderedExhibitNames");
-        orderedEdgeList = (List<GraphPath>) intent.getSerializableExtra("orderedEdgeList");
 
-        prevNode = intent.getStringExtra("prevNode");
+        directionData = new DirectionData((List<GraphPath>) intent.getSerializableExtra("orderedEdgeList"),
+                intent.getStringArrayListExtra("orderedExhibitNames"));
+
+
+        Log.d("Exhibit List",directionData.orderedExhibitNames.toString());
+        Log.d("Edge List", directionData.orderedEdgeList.toString());
 
         // 1. Load the graph...
         Graph<String, IdentifiedWeightedEdge> g = ZooData.loadZooGraphJSON("sample_zoo_graph.json", this);
@@ -43,62 +48,32 @@ public class DirectionDetailsActivity extends AppCompatActivity {
         Map<String, ZooData.VertexInfo> vInfo = ZooData.loadVertexInfoJSON("sample_node_info.json", this);
         Map<String, ZooData.EdgeInfo> eInfo = ZooData.loadEdgeInfoJSON("sample_edge_info.json", this);
 
-        DirectionAdapter adapter = new DirectionAdapter();
+        directionData.addGraphs(g, vInfo, eInfo);
+
+        adapter = new DirectionAdapter();
 
         textView = findViewById(R.id.directionDetails);
-        textView.setText(orderedExhibitNames.get(0));
 
-        System.out.println("Direction details: " + orderedEdgeList.size());
+        System.out.println("Direction details: " + directionData.orderedEdgeList.size());
 
-        /*for (GraphPath currPath : orderedEdgeList) {
-            System.out.println(currPath.getEdgeList());
-            for (Object edge : currPath.getEdgeList()) {
-                System.out.println(g.getEdgeWeight((IdentifiedWeightedEdge) edge));
-            }
-        }*/
-        List<MockIndividualEdge> myList = new ArrayList<MockIndividualEdge>();
-        List<IdentifiedWeightedEdge> edgePath = orderedEdgeList.get(0).getEdgeList();
-        //List<String> nodePath = orderedEdgeList.get(0).getVertexList();
-        for (int i = 0; i < edgePath.size(); i++) {
-            String target =  vInfo.get(g.getEdgeTarget(edgePath.get(i)).toString()).name;
-            if(target.equals(prevNode)){
-                myList.add(new MockIndividualEdge(eInfo.get(edgePath.get(i).getId()).street, vInfo.get(g.getEdgeSource(edgePath.get(i)).toString()).name, g.getEdgeWeight(edgePath.get(i))));
-            } else {
-                myList.add(new MockIndividualEdge(eInfo.get(edgePath.get(i).getId()).street, vInfo.get(g.getEdgeTarget(edgePath.get(i)).toString()).name, g.getEdgeWeight(edgePath.get(i))));
-            }
-            if (i == edgePath.size() - 1) {
-                if(prevNode != vInfo.get(g.getEdgeTarget(edgePath.get(i)).toString()).name){
-                    textView.setText(vInfo.get(g.getEdgeTarget(edgePath.get(i)).toString()).name);
-                } else{
-                    textView.setText(vInfo.get(g.getEdgeSource(edgePath.get(i)).toString()).name);
-                }
-            }
-            prevNode = myList.get(i).getNodeTo();
-        }
-        orderedExhibitNames.remove(0);
-        orderedEdgeList.remove(0);
 
         recyclerView = findViewById(R.id.direction_lists);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(adapter);
-
-        adapter.setIndividualDirectionListItems(myList);
-
+        adapter.setIndividualDirectionListItems(directionData.getCurrentExhibitDirections());
+        textView.setText(directionData.getTitleText());
     }
 
     public void onNextClicked(View view) {
-        // Call this activity again with shortened version of list (not including current exhibit/path)
-        //  pass shortened list
-        if(orderedEdgeList.size()>0){
-            Intent intent = new Intent(this, DirectionDetailsActivity.class);
-            intent.putExtra("orderedEdgeList", (Serializable) orderedEdgeList);
-            intent.putStringArrayListExtra("orderedExhibitNames", (ArrayList<String>) orderedExhibitNames);
-            intent.putExtra("prevNode", prevNode);
-            startActivity(intent);
+
+        if(directionData.currentExhibitIndex < directionData.orderedEdgeList.size()){
+
+            adapter.setIndividualDirectionListItems(directionData.getCurrentExhibitDirections());
+            textView.setText(directionData.getTitleText());
         }
-        finish();
-
-
+        else{
+            finish();
+        }
 
     }
 }
